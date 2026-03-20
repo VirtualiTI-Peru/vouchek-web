f7f690a452430ac42b1a4264ac6239f776205cc1
jlozano-virtualiti <javier.lozano@virtualiti.com.pe>
2026-03-20 12:02:19 -0500

    one more

--- app/welcome/page.tsx
+++ app/welcome/page.tsx
@@ -1,7 +1,7 @@
 export default function Welcome() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-4xl font-bold mb-4">Bienvenido a VouCheck!</h1>
+      <h1 className="text-4xl font-bold mb-4">Bienvenido a VouChek!</h1>
       <p className="text-lg">Pronto recibirá un correo electrónico con más información.</p>
     </main>
   );
